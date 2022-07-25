e8af2dacbca9af6045f6748f01e3f8569ce429de
Anwar Sagor <sagor.onnet@gmail.com>
2022-07-25 18:37:35 +0600

    .env & .gitignore

--- index.js
+++ index.js
@@ -1,16 +1,24 @@
 const express = require('express')
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express()
 const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.znkbe.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello Tools!')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Tools port ${port}`)
 })
